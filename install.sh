215e0e889aa2bec52002e120192766fb6ec7b5bc
dingboopt <dingboopt@gmail.com>
2017-01-18 16:00:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -x
 sudo rm -rf master*
 sudo rm -rf  wqq*
-wget https://github.com/dingboopt/neutron-x/archive/test.zip --no-check-certificate -e use_proxy=yes -e https_proxy=https://d00327060:zxcv13%40%24@proxyhk.huawei.com:8080/
+wget https://github.com/dingboopt/neutron-x/archive/test.zip --no-check-certificate -e use_proxy=yes -e https_proxy=https://name:key@proxyhk.huawei.com:8080/
 unzip master.zip
 cd wqq-master
 git init
